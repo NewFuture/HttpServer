6fe9fe9e8f52197ebe9870f6ec7648ddfecb276c
New Future <github@newfuture.cc>
2017-01-04 11:34:03 +0800

    重命名

--- HTTPServer/Properties/AssemblyInfo.cs
+++ HTTPServer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("HTTPServer")]
+[assembly: AssemblyTitle("HttpServerLib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HTTPServer")]
+[assembly: AssemblyProduct("HttpServerLib")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("c0256988-62e5-447f-b056-09288e77950e")]
+[assembly: Guid("ed455757-2cb9-4206-bb94-0db98ce9c917")]
 
 // 程序集的版本信息由下面四个值组成:
 //
